96d1766357733c5438ea8f98ae7411745e6f698d
APURBA ROY <apurbaanikroy@gmail.com>
2021-02-02 00:36:05 +0600

    kail bakita kormu khub thanda

---
+++ doubleVsTriple-equal.js
@@ -0,0 +1,8 @@
+const first = 2;
+const second = "3";
+if(first === second){
+    console.log("COndition is true");
+}
+else {
+    console.log("condition is false");
+}
---
+++ null-vs-undefined.js
@@ -0,0 +1,6 @@
+// let pakhi;
+// function add(num1,num2){
+    // console.log(num1+num2);
+// }
+const result = add(13,82);
+console.log(result);
