a1855caff1b90179d7d0243010c504a4e3dc09c1
APURBA ROY <apurbaanikroy@gmail.com>
2021-02-01 23:00:09 +0600

    kamne true hoy ar kamne false hoy

---
+++ trueFalse.js
@@ -0,0 +1,9 @@
+//IF the value is 0 then false otherwise the value will be true & if we take 0 as string then it will be true
+const age = 4; 
+const name ="Solaiman"
+if(){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is false");
+}
